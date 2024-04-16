bdaedeb20e6c6ed1ea6b3e657f71a7221485af9d
Stephen <stephensellers034@Stephens-MacBook-Air.local>
2024-04-16 09:57:09 -0400

    PalindromeExercise my work

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+		public bool IsAPalindrome(string palin)
+		{
+			var reversed = "";
+
+			for(int i = palin.Length - 1; i >= 0; i--)
+			{
+				reversed += palin.ToLower()[i];
+			}
+			if(reversed == palin)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}	
+	}
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,32 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("madam", true)]
+        [InlineData("radar", true)]
+        [InlineData("noon", true)]
+        [InlineData("deified", true)]
+        [InlineData("dad", true)]
+        [InlineData("mom", true)]
+        [InlineData("face", false)]
+        [InlineData("track", false)]
+        [InlineData("dog", false)]
+        public void IsAPalindrome(string palin, bool expected)
         {
+            //Arrange
+            WordSmith palindrome = new WordSmith();
 
+            //Act
+          bool actual =  palindrome.IsAPalindrome(palin);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
